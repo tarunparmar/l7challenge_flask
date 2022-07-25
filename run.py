738a12d1d67e980f0160b1ee52205174e6d73573
Tarun Parmar <tarunparmar@gmail.com>
2022-07-24 19:18:22 -0500

    update run.py

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from flask import Flask
-from l7challenge_flask import app
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+from l7challenge_flask import app as application
+#if __name__ == "__main__":
+#    app.run(debug=True, host='0.0.0.0')
+app = application
